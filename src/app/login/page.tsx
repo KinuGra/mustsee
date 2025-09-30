0aa51ae18553b62abcc4b3048fd1994f6e63d7de
air_kinu <techgit1024@gmail.com>
2025-09-30 23:55:29 +0900

    add: ログインページを最低限作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => {
+  return (
+    <>
+      <form>
+        <input type="email" placeholder="mail"></input>
+        <input type="password" placeholder="password"></input>
+      </form>
+    </>
+  );
+};
+
+export default LoginPage;
