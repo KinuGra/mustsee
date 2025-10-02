051aa05af69eebdc52f08a5e6cfb4a66b67bc0e5
air_kinu <techgit1024@gmail.com>
2025-10-02 23:57:54 +0900

    add: (wip)(#3)ログインページ作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,29 @@
+import styles from "./Login.module.css";
+
 const LoginPage = () => {
   return (
     <>
-      <form>
-        <input type="email" placeholder="mail"></input>
-        <input type="password" placeholder="password"></input>
-      </form>
+      <div>
+        <form className={styles.card}>
+          <div>
+            <input
+              className={styles.field}
+              type="email"
+              placeholder="mail"
+            ></input>
+          </div>
+          <div>
+            <input
+              className={styles.field}
+              type="password"
+              placeholder="password"
+            ></input>
+          </div>
+          <div className={styles.button}>
+            <button type="button">ログイン</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
