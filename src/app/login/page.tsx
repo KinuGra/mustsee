e1a679602209eef78d7c0348f6a5e9094e354c91
air_kinu <techgit1024@gmail.com>
2025-10-29 18:40:51 +0900

    feat: ログイン成功時に/chatに遷移する機能を実装

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,51 +4,53 @@ import { auth } from "@/lib/FirebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import styles from "./Login.module.css";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter();
 
-  const doSignIn = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        alert("ログイン完了");
-        console.log(user);
-      })
-      .catch((error) => {
-        console.error("ログイン失敗:", error);
-      });
-  };
+	const doSignIn = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				console.log(user);
+				router.push("/chat");
+			})
+			.catch((error) => {
+				console.error("ログイン失敗:", error);
+			});
+	};
 
-  return (
-    <>
-      <div className={styles.card}>
-        <h1>ログイン</h1>
-        <form>
-          <div className={styles.field}>
-            <input
-              type="email"
-              placeholder="mail"
-              onChange={(event) => setEmail(event.target.value)}
-            ></input>
-          </div>
-          <br />
-          <div className={styles.field}>
-            <input
-              type="password"
-              placeholder="password"
-              onChange={(event) => setPassword(event.target.value)}
-            ></input>
-          </div>
-          <br />
-          <button type="button" className={styles.button} onClick={doSignIn}>
-            ログイン
-          </button>
-        </form>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.card}>
+				<h1>ログイン</h1>
+				<form>
+					<div className={styles.field}>
+						<input
+							type="email"
+							placeholder="mail"
+							onChange={(event) => setEmail(event.target.value)}
+						></input>
+					</div>
+					<br />
+					<div className={styles.field}>
+						<input
+							type="password"
+							placeholder="password"
+							onChange={(event) => setPassword(event.target.value)}
+						></input>
+					</div>
+					<br />
+					<button type="button" className={styles.button} onClick={doSignIn}>
+						ログイン
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default LoginPage;
